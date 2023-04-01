5d12dd92333598fe8e6be3bca8b7785daa72d0c3
matt24122 <mahit.choubey@students.mq.edu.au>
2023-04-01 18:32:05 +1100

    added and fixed job scheduling

--- src/pre-compiled/MyClient.java
+++ src/pre-compiled/MyClient.java
@@ -20,10 +20,12 @@ class MyClient {
         /*System.out.println("Server says: " + response);*/
 
     
-       	Boolean flag=true;
+       	Boolean flag = true;
         String largestServerType = null;
-        int largestCoreCount=0;
+        int largestCoreCount = 0;
         int largestServerCount = 1;
+        
+        int serverID = 0;
         
      
       while (true) {
@@ -31,7 +33,7 @@ class MyClient {
       	 //System.out.println("Value at start of loop: " + response); //Checking server response
          out.println("REDY"); // Send REDY
          response = in.readLine(); // Receive a message
-        // System.out.println("First while loop response after REDY: " + response); //Checking server response
+         System.out.println("First while loop response after REDY: " + response); //Checking server response
         
             
       	if (response.equals("NONE")){ //Break out of the loop if no jobs available
@@ -44,6 +46,15 @@ class MyClient {
 	
 	else { //Server response is JOBN
 	
+		//Store job details:
+		String[] jobInfo = response.split("\\s+");
+                int jobID = Integer.parseInt(jobInfo[2]);
+                int estRunTime = Integer.parseInt(jobInfo[3]);
+                int core = Integer.parseInt(jobInfo[4]);
+                int mem = Integer.parseInt(jobInfo[5]);
+                System.out.println("Job details: " +  Arrays.toString(jobInfo));
+	
+		//Check server info only ONCE?:
 		if (flag) {
 		 out.println("GETS All"); //Get server state information
 		 response=in.readLine(); //Should respond with DATA X Y
@@ -61,10 +72,10 @@ class MyClient {
                     response = in.readLine(); //Receiving each record
                     
                     
-                    System.out.println("RECENT" + response);
+                    System.out.println(response); //Checking record info
+                    
                     String[] serverInfo = response.split("\\s+");
                     String serverType = serverInfo[0];
-                  //  int serverCount = Integer.parseInt(serverInfo[1]);
                     int coreCount = Integer.parseInt(serverInfo[4]);
                     
                      if (serverType.equals(largestServerType)) { //Checking if there is more of the largest server type
@@ -72,91 +83,38 @@ class MyClient {
         
                 } 
                    
-                    if (coreCount > largestCoreCount){ 	
-                    	 largestServerType = serverType;
-                         largestCoreCount= coreCount;
+                    if (coreCount > largestCoreCount){ 	//Checking if there is a larger server type
+                    	 largestServerType = serverType; //Updating largest server type 
+                         largestCoreCount= coreCount; //Updating largest core count value
                          largestServerCount=1; // Resetting count 1 if new larger count is found
-                
-		}  
-		
-		   
-	}
+		}   
+	     }
+	     
+	     out.println("OK"); //Send OK
+	     response= in.readLine();
+	
 	}
 	
+	flag=false; //Determining largest server only ONCE?
+	
+	if (serverID >= largestServerCount){
+		serverID=0; //resetting to 0
+	}
+	//Scheduling jobs:
+	out.println("SCHD " + jobID + " "+ largestServerType + " "+ serverID);
+	System.out.println("Job assigned to: " + serverID); //Checking server ID
+	serverID++; //Incrementing server ID to assign next job to next server
+	response= in.readLine();
+	
+	
+	//Checking largest server info:
 	System.out.println("LargServ: " + largestServerType);
 	System.out.println("LargeCCount: " + largestCoreCount);
 	System.out.println("SERVCOUNT: " + largestServerCount);
+	
+		}
 	}
       
-      
-      
-      
-      
-       /*
-        while (!response.equals("NONE")) {
-            
-   	    System.out.println("Value at start of loop: " + response);
-            out.println("REDY"); // Send REDY
-            response = in.readLine(); // Receive a message
-            System.out.println("First while loop response after REDY: " + response);
- 
-            if (response.startsWith("JOBN")) {
-                // Schedule a job
-                
-                String[] parts = response.split("\\s+");
-                int jobID = Integer.parseInt(parts[2]);
-                int estRunTime = Integer.parseInt(parts[3]);
-                int core = Integer.parseInt(parts[4]);
-                int mem = Integer.parseInt(parts[5]);
-                out.println("SCHD " + jobID + " " + largestServerType + " 0"); // Schedule on largest server type
-      
-            } 
-            
-           	else {
-           	
-                // Identify the largest server type
-                out.println("GETS All");
-                // set a single gets and complete for a server at a time
-                // using a for loop use a single
-                response = in.readLine();
-                System.out.println("Response: " + response);
-           
-                String[] parts = response.split("\\s+");
-                
-                System.out.println("Parts: " + Arrays.toString(parts));
-                
-                int nRecs = Integer.parseInt(parts[0]);
-                int recSize = Integer.parseInt(parts[1]);
-                
-               
-                
-                out.println("OK");
-                
-                int maxCount = 0;
-                for (int i = 0; i < nRecs; i++) {
-             
-                    response = in.readLine();
-                    
-                    if (response == null) {
-                    	break;
-                    }
-                    
-                    System.out.println("RECENT" + response);
-                    String[] serverParts = response.split("\\s+");
-                    String serverType = serverParts[0];
-                    int serverCount = Integer.parseInt(serverParts[1]);
-                    
-                    	if (serverCount > maxCount) {
-                        largestServerType = serverType;
-                        largestServerCount = serverCount;
-                        maxCount = serverCount;
-                    }
-                } 
-              
-                out.println("OK");
-            }
-        }*/
-
         out.println("QUIT"); // Send QUIT 
     
         response = in.readLine(); // Receive QUIT
@@ -164,10 +122,10 @@ class MyClient {
         in.close();
         out.close();
         s.close();
-    }
+   }
     
-    }
-    }
+ }
+  
     
     
   
