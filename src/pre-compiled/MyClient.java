f402edd68ce8f79490cfaff883d9f522e788c1e9
matt24122 <mahit.choubey@students.mq.edu.au>
2023-04-03 15:04:43 +1000

    Fixed code indentation + removed unnecessary spaces

--- src/pre-compiled/MyClient.java
+++ src/pre-compiled/MyClient.java
@@ -32,78 +32,82 @@ class MyClient {
          out.println("REDY"); // Send REDY to request for job
          sreply = in.readLine(); // Receive a message
         
-      	if (sreply.equals("NONE")){ //Break out of the loop if no jobs available
-		break;
-	}
+      		if (sreply.equals("NONE")){ //Break out of the loop if no jobs available
+		   break;
+		}
 	
-	else if (sreply.startsWith("JCPL")) { //Next iteration of loop after job completion message
-		continue;
-	}
+		else if (sreply.startsWith("JCPL")) { //Next iteration of loop after job completion message
+		  continue;
+		}
 	
-	else { //Server response is JOBN; Start scheduling process + determine largest server (only first loop iteration)
+		else { //Server response is JOBN; Start scheduling process + determine largest server (only first loop iteration)
 	
 		//Store job details:
-		String[] jobInfo = sreply.split("\\s+");
-                int jobID = Integer.parseInt(jobInfo[2]); //job ID is the only important value needed for scheduling
+		  String[] jobInfo = sreply.split("\\s+");
+                  int jobID = Integer.parseInt(jobInfo[2]); //job ID is the only important value needed for scheduling
              
 	
-		//Check server info only ONCE:
-		if (flag) {
-		 out.println("GETS All"); //Get server state information
-		 sreply=in.readLine(); //Should respond with DATA X Y
-		 out.println("OK"); //Send OK 
-		 
-		String[] parts = sreply.split("\\s+"); //Storing DATA X Y response to determine nRecs and recSize
-                int nRecs = Integer.parseInt(parts[1]); 
-                int recSize = Integer.parseInt(parts[2]);
+			//Check server info only ONCE:
+			if (flag) {
+			 out.println("GETS All"); //Get server state information
+			 sreply=in.readLine(); //Should respond with DATA X Y
+			 out.println("OK"); //Send OK 
+			 
+			String[] parts = sreply.split("\\s+"); //Storing DATA X Y response to determine nRecs and recSize
+		        int nRecs = Integer.parseInt(parts[1]); 
+		        int recSize = Integer.parseInt(parts[2]);
                
-              
-                for (int i = 0; i < nRecs; i++) {
-             
-                    sreply = in.readLine(); //Receiving each record
-                    String[] serverInfo = sreply.split("\\s+");
-                    String serverType = serverInfo[0];
-                    int coreCount = Integer.parseInt(serverInfo[4]);
-                    
-                     if (serverType.equals(largestServerType)) { //Checking if there is more of the largest server type
-                    		largestServerCount++; //Incrementing count if current server type equals to largest
-        
-                } 
+				for (int i = 0; i < nRecs; i++) {
+			     
+				    sreply = in.readLine(); //Receiving each record
+				    String[] serverInfo = sreply.split("\\s+");
+				    String serverType = serverInfo[0];
+				    int coreCount = Integer.parseInt(serverInfo[4]);
+				    
+				     if (serverType.equals(largestServerType)) { //Checking if there is more of the largest server type
+				    		largestServerCount++; //Incrementing count if current server type equals to largest
+				} 
                    
-                    if (coreCount > largestCoreCount){ 	//Checking if there is a larger server type
-                    	 largestServerType = serverType; //Updating largest server type 
-                         largestCoreCount= coreCount; //Updating largest core count value
-                         largestServerCount=1; // Resetting count 1 if new larger count is found
-		}   
+				    if (coreCount > largestCoreCount){ 	//Checking if there is a larger server type
+				    	 largestServerType = serverType; //Updating largest server type 
+				         largestCoreCount= coreCount; //Updating largest core count value
+				         largestServerCount=1; // Resetting count 1 if new larger count is found
+				}   
+	     		}
+	     
+	    		  out.println("OK"); //Send OK
+	                  sreply= in.readLine();
+	
+		    }
+	
+		flag=false; //Determining largest server only ONCE
+	
+		if (serverID >= largestServerCount){
+			serverID=0; //resetting to 0
+		}
+	
+		//Scheduling jobs:
+		out.println("SCHD " + jobID + " "+ largestServerType + " "+ serverID);
+		
+		//System.out.println("Job assigned to: " + serverID); //Checking job assigment and server ID's
+		
+		serverID++; //Incrementing server ID to assign next job to next server
+		sreply= in.readLine();
+		
 	     }
 	     
-	     out.println("OK"); //Send OK
-	     sreply= in.readLine();
-	
-	}
-	
-	flag=false; //Determining largest server only ONCE
-	
-	if (serverID >= largestServerCount){
-		serverID=0; //resetting to 0
-	}
-	
-	//Scheduling jobs:
-	out.println("SCHD " + jobID + " "+ largestServerType + " "+ serverID);
-	//System.out.println("Job assigned to: " + serverID); //Checking job assigment and server ID's
-	serverID++; //Incrementing server ID to assign next job to next server
-	sreply= in.readLine();
-		}
 	}
       
-        out.println("QUIT"); // Send QUIT
+        out.println("QUIT"); // Send QUIT to server
     
-        sreply = in.readLine(); // Receive QUIT
+        sreply = in.readLine(); // Receive QUIT from server
 
         in.close();
         out.close();
         s.close();
+        
    }
+   
     
  }
   
