468a33aefc322bfa339e016434576b8356804286
mahit-c <mahit.choubey@gmail.com>
2023-05-27 20:54:18 +1000

    Added modifications for first fit

--- src/pre-compiled/Stage2Client.java
+++ src/pre-compiled/Stage2Client.java
@@ -17,15 +17,17 @@ class Stage2Client {
        String username = "mahit";
        String authMessage = "AUTH " + username;
        out.println(authMessage); // Client sends AUTH username
-
+ 
        sreply = in.readLine(); //Server should respond OK
     
-       Boolean flag = true;  //For getting largest server info only ONCE
-       String largestServerType = null; //For storing the largest server type name
-       int largestCoreCount = 0; //For storing the server's core counts to determine the highest (largest) one
-       int largestServerCount = 1; //For keeping track of the no.of servers of the largest type
-       int serverID = 0; //For scheduling jobs to the required servers
+       //Boolean flag = true;  //For getting largest server info only ONCE
+       //String largestServerType = null; //For storing the largest server type name
+       //int largestCoreCount = 0; //For storing the server's core counts to determine the highest (largest) one
+       //int largestServerCount = 1; //For keeping track of the no.of servers of the largest type
+       //int serverID = 0; //For scheduling jobs to the required servers
         
+    	String serverToSend = null;
+    	String serverID=null;
      
        while (true) {
       
@@ -45,57 +47,73 @@ class Stage2Client {
 		//Store job details:
 		  String[] jobInfo = sreply.split("\\s+");
                   int jobID = Integer.parseInt(jobInfo[2]); //job ID is the only important value needed for scheduling
+                  int core = Integer.parseInt(jobInfo[4]);
+                  int memory = Integer.parseInt(jobInfo[5]);
+                  int disk = Integer.parseInt(jobInfo[5]);
+                  
+                  System.out.println("Job Details are:" + core + memory + disk);
+                  
+             //Check currently available servers using GETS Avail:
              
-			//Check server info only ONCE:
-			if (flag) {
-			 out.println("GETS All"); //Get server state information
+			 out.println("GETS Avail " + core + " "+ memory + " "+ disk); 
 			 sreply=in.readLine(); //Should respond with DATA X Y
+			 
+			 if (sreply.equals("0")) {
+			 	System.out.println("Server responded 0");
+			 }
+			 System.out.println("Server response to GETS AVAIL:" + sreply);
+                  
+			 
+			 //
 			 out.println("OK"); //Send OK 
 			 
 			 String[] parts = sreply.split("\\s+"); //Storing DATA X Y response to determine nRecs and recSize
+			
+                  
 		         int nRecs = Integer.parseInt(parts[1]); 
 		         int recSize = Integer.parseInt(parts[2]);
-               
-				for (int i = 0; i < nRecs; i++) {
+		         
+               //Scheduling job to First AVAILABLE SERVER:
+				
+				for (int i=0; i<nRecs; i++) {
 			     
 				    sreply = in.readLine(); //Receiving each record
+				    System.out.println("SERVER INFO REPLY: " + sreply);
 				    String[] serverInfo = sreply.split("\\s+");
-				    String serverType = serverInfo[0];
-				    int coreCount = Integer.parseInt(serverInfo[4]);
 				    
-				     if (serverType.equals(largestServerType)) { //Checking if there is more of the largest server type
-				    		largestServerCount++; //Incrementing count if current server type equals to largest
-				     } 
+				    if (i==0) {
+				    serverToSend = serverInfo[0];
+				    serverID = serverInfo[1];
+				    }
+				    
+				   }
+				    
                    
-				     if (coreCount > largestCoreCount){ 	//Checking if there is a larger server type
-				    	 largestServerType = serverType; //Updating largest server type 
-				         largestCoreCount= coreCount; //Updating largest core count value
-				         largestServerCount=1; // Resetting count 1 if new larger count is found
-				     }   
-	     		         }
+
 	     
-	    		  out.println("OK"); //Send OK
-	                  sreply= in.readLine();
+	    		//out.println("OK"); //Send OK
+	                //sreply= in.readLine();
 	
-		       }
+		       
 	
-		flag=false; //Determining largest server only ONCE
-	
-		if (serverID >= largestServerCount){
-			serverID=0; //resetting to 0
-		}
+		
 	
 		//Scheduling jobs:
-		out.println("SCHD " + jobID + " "+ largestServerType + " "+ serverID);
+		out.println("SCHD " + jobID + " "+ serverToSend + " "+ serverID);
 		
-		//System.out.println("Job assigned to: " + serverID); //Checking job assigment and server ID's
+		System.out.println("SCHD " + jobID + " "+ serverToSend + " "+ serverID); //Checking job assigment and server ID's
 		
-		serverID++; //Incrementing server ID to assign next job to next server
 		sreply= in.readLine();
+		
+		System.out.println("Server response after SCHD " + sreply);
+		
+		
 		
 	     }
 	     
 	  }
+	  
+	
       
         out.println("QUIT"); // Send QUIT to server
     
@@ -106,9 +124,13 @@ class Stage2Client {
         s.close();
         
      }
+     
+    }
+    
+   
    
     
-}
+
   
     
     
