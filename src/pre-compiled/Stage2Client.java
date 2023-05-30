45fbc2f1dd96e92e33ce0074baa4744b8878efa4
mahit-c <mahit.choubey@gmail.com>
2023-05-30 14:22:54 +1000

    Final modifications to algorithm

--- src/pre-compiled/Stage2Client.java
+++ src/pre-compiled/Stage2Client.java
@@ -6,22 +6,22 @@ class Stage2Client {
     public static void main(String args[]) throws Exception {
     
     	//Initial connection and communication establishment:
-       Socket s = new Socket("127.0.0.1", 50000);
-       BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-       PrintWriter out = new PrintWriter(s.getOutputStream(), true);
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Socket s = new Socket("127.0.0.1", 50000);
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-       out.println("HELO"); // Client sends HELO
-       String sreply = in.readLine(); //Storing server responses //Server should respond with OK here
+        out.println("HELO"); // Client sends HELO
+        String sreply = in.readLine(); //Storing server responses //Server should respond with OK here
 	
-       String username = "mahit";
-       String authMessage = "AUTH " + username;
-       out.println(authMessage); // Client sends AUTH username
+        String username = "mahit";
+        String authMessage = "AUTH " + username;
+        out.println(authMessage); // Client sends AUTH username
  
-       sreply = in.readLine(); //Server should respond OK
+       	sreply = in.readLine(); //Server should respond OK
         
-    	String serverToSend = null;
-    	String serverID=null;
+    	String serverToSend = null; //Storing server to schedule to type
+    	String serverID=null; //Storing server to schedule to ID
     	
      
        while (true) {
@@ -39,85 +39,66 @@ class Stage2Client {
 	
 		else { //Server response is JOBN; Start scheduling process + determine largest server (only first loop iteration)
 	
-		//Store job details:
-		  String[] jobInfo = sreply.split("\\s+");
-		 // System.out.println("JOBINFO ARRAY: " + jobInfo[0]);
-                  int jobID = Integer.parseInt(jobInfo[2]); //job ID is the only important value needed for scheduling
-                  int core = Integer.parseInt(jobInfo[4]);
-                  int memory = Integer.parseInt(jobInfo[5]);
-                  int disk = Integer.parseInt(jobInfo[6]);
+			//Store job details:
+			  String[] jobInfo = sreply.split("\\s+");
+			 // System.out.println("JOBINFO ARRAY: " + jobInfo[0]);
+		          int jobID = Integer.parseInt(jobInfo[2]); //job ID is the only important value needed for scheduling
+		          int core = Integer.parseInt(jobInfo[4]);
+		          int memory = Integer.parseInt(jobInfo[5]);
+		          int disk = Integer.parseInt(jobInfo[6]);
+		          
+		   	  boolean check= false; //Variable to control GETS Capable scheduling
                   
-                  boolean check= false; //Variable to control GETS Capable scheduling
-                  
-                 // System.out.println("Job Details are:" + core + memory + disk);
-                  
-             //Check currently available servers using GETS Avail:
+            		 //Check currently available servers using GETS Avail:
              
 			 out.println("GETS Avail " + core + " "+ memory + " "+ disk); 
 			 sreply=in.readLine(); //Should respond with DATA X Y
 			 
 			
 			 //System.out.println("Server response to GETS AVAIL:" + sreply);
-                  
-			 
-			 //
 			 out.println("OK"); //Send OK 
 			 
 			 String[] parts = sreply.split("\\s+"); //Storing DATA X Y response to determine nRecs and recSize
-			
-                  
+		
 		         int nRecs = Integer.parseInt(parts[1]); 
 		         int recSize = Integer.parseInt(parts[2]);
 		         
 		          if (nRecs==0) { //If NO Available servers go to servers that have jobs waiting
 		          	
 		      	
-			 	//System.out.println("Server responded 0");
 			 	sreply=in.readLine();
-			 	//System.out.println("First reply inside the if = " + sreply);
-			 	
-			 	//System.out.println("Server reply after DATAXY OK = " + sreply);
 			 	out.println("OK"); //Send OK 
 			 	sreply=in.readLine(); 
 			 	//System.out.println("Server reply after OK = " + sreply); //Should be . here
-	
-			 
+			 	
+				//Requesting list of capable servers:
+				
 			 	out.println("GETS Capable " + core + " "+ memory + " "+ disk); 
-			 	
-			 	//System.out.println("GETS Capable " + core + " "+ memory + " "+ disk);  //Checking for CAPABLE SERVERS (which can eventually do it);
-			 	
 			 	sreply = in.readLine();
 			 	sreply = in.readLine();
 			 	//System.out.println( "Reply after GETS Capable: " + sreply);
-			 	
 			 	out.println("OK"); //Send OK after DATA X Y
 			 	
+			 	//Updating server list previously found:
 			 	String [] newParts = sreply.split("\\s+");
 			 	nRecs = Integer.parseInt(newParts[1]); //Updating value of nRecs
-			 	//System.out.println("New Value of NRECS: " + nRecs);
-			 	
-			 	
+		 	
 			 	//Setting check to true:
 			 	check =  true;
-			 	
-
-			 	
 			 } 
 		         
-				
 				for (int i=0; i<nRecs; i++) {
 					
 			     	    int smallestValue=0; //for storing no.of waiting jobs
 			     	    
 				    sreply = in.readLine(); //Receiving each record
-				  //  System.out.println("SERVER INFO REPLY: " + sreply);
 				    String[] serverInfo = sreply.split("\\s+");
 				    
 				    if (i==0) {
 				    serverToSend = serverInfo[0];
 				    serverID = serverInfo[1];
 				    smallestValue = Integer.parseInt(serverInfo[7]); //Storing no.of waiting jobs
-				   System.out.println("The first server to schedule is: " + serverToSend +   serverID + Integer.parseInt(serverInfo[7]));
+				  //System.out.println("The first server to schedule is: " + serverToSend +   serverID + Integer.parseInt(serverInfo[7]));
 				    }
 				    
 				    
@@ -130,26 +111,14 @@ class Stage2Client {
 					    	serverID = serverInfo[1];
 					    	check = false;
 				    	}
-					   System.out.println("The new server to schedule is: " + serverToSend +   serverID + Integer.parseInt(serverInfo[7]));
-
-				    
 				    }
-				  
 				    
-				    
-				   }
-				    
-				    
-				      
-	     
+				 }
                    
-	    		out.println("OK"); //Send OK
-	                sreply= in.readLine();
+		    		out.println("OK"); //Send OK
+			        sreply= in.readLine();
 	
-		       
-	
-		
-	
+
 		//Scheduling jobs:
 		out.println("SCHD " + jobID + " "+ serverToSend + " "+ serverID);
 		
@@ -158,8 +127,6 @@ class Stage2Client {
 		sreply= in.readLine();
 		
 		//System.out.println("Server response after SCHD " + sreply);
-		
-		//}
 		
 	     }
 	     
